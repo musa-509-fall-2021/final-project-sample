6bfa1f6d61aa1c18906741a855da0355e9164fe7
Mjumbe Poe <mjumbewu@gmail.com>
2021-11-16 19:06:21 -0500

    Use the updated overview table in the rendered report

--- airflow/dags/report_generator/generate_report.py
+++ airflow/dags/report_generator/generate_report.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 import pandas as pd
 from jinja2 import Environment, PackageLoader
 
-def main(**kwargs):
+def main():
     corridors_df = pd.read_gbq('SELECT * FROM final.corridors')
     corridors_df.geog = gpd.GeoSeries.from_wkt(corridors_df.geog)
     corridors_gdf = gpd.GeoDataFrame(corridors_df, geometry='geog')
@@ -11,9 +11,11 @@ def main(**kwargs):
 
     env = Environment(loader=PackageLoader('generate_report'))
     template = env.get_template('index.html')
-    print(template.render(
-        corridors=corridors_gdf.to_dict('records')
-    ))
+    output = template.render(
+        corridors=corridors_gdf.to_dict('records'),
+        overview=overview_df.to_dict('records')[0],
+    )
+    return output
 
 if __name__ == '__main__':
-    main()
+    print(main())
